afd356871311bf847b068004c4519cd1d579b16c
vadzim.l <vadzim.l@realmond.com>
2024-09-08 19:28:29 +0300

    add: `instalment.mdx` template

--- src/app/(content)/instalment/page.tsx
+++ src/app/(content)/instalment/page.tsx
@@ -1,7 +1,13 @@
 import type { Page } from '~/lib/types'
 
+import InstallmentMarkdown from './instalment.mdx'
+
 const InstalmentPage: Page = () => {
-	return <section></section>
+	return (
+		<section>
+			<InstallmentMarkdown />
+		</section>
+	)
 }
 
 export default InstalmentPage

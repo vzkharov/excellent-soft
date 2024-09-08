cb4536eaf1e0cb77508d83bd09e6117631dfce86
vadzim.l <vadzim.l@realmond.com>
2024-09-08 19:25:19 +0300

    update: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,15 +2,49 @@ import type { MetadataRoute } from 'next'
 
 import { DOMAIN_URL } from '~/env'
 
-const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
-	return [
-		{
-			url: `${DOMAIN_URL}`,
-			lastModified: new Date(),
-			changeFrequency: 'yearly',
-			priority: 1,
-		},
-	]
-}
+const sitemap = (): MetadataRoute.Sitemap => [
+	{
+		url: `${DOMAIN_URL}`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 1,
+	},
+	{
+		url: `${DOMAIN_URL}/works`,
+		lastModified: new Date(),
+		changeFrequency: 'weekly',
+		priority: 0.4,
+	},
+	{
+		url: `${DOMAIN_URL}/services/development`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 0.6,
+	},
+	{
+		url: `${DOMAIN_URL}/services/design`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 0.6,
+	},
+	{
+		url: `${DOMAIN_URL}/services/promotion`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 0.6,
+	},
+	{
+		url: `${DOMAIN_URL}/instalment`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+		priority: 0.2,
+	},
+	{
+		url: `${DOMAIN_URL}/contact`,
+		lastModified: new Date(),
+		changeFrequency: 'yearly',
+		priority: 0.1,
+	},
+]
 
 export default sitemap
--- src/components/header/header-nav.tsx
+++ src/components/header/header-nav.tsx
@@ -3,8 +3,6 @@
 import * as React from 'react'
 import Link from 'next/link'
 
-import { cn } from '~/lib/utils'
-
 import {
 	NavigationMenu,
 	NavigationMenuContent,
@@ -14,43 +12,6 @@ import {
 	NavigationMenuTrigger,
 	navigationMenuTriggerStyle,
 } from '~/components/ui/navigation-menu'
-import { Logo } from '../logo'
-
-const components: { title: string; href: string; description: string }[] = [
-	{
-		title: 'Alert Dialog',
-		href: '/docs/primitives/alert-dialog',
-		description: 'A modal dialog that interrupts the user with important content and expects a response.',
-	},
-	{
-		title: 'Hover Card',
-		href: '/docs/primitives/hover-card',
-		description: 'For sighted users to preview content available behind a link.',
-	},
-	{
-		title: 'Progress',
-		href: '/docs/primitives/progress',
-		description:
-			'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-	},
-	{
-		title: 'Scroll-area',
-		href: '/docs/primitives/scroll-area',
-		description: 'Visually or semantically separates content.',
-	},
-	{
-		title: 'Tabs',
-		href: '/docs/primitives/tabs',
-		description:
-			'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-	},
-	{
-		title: 'Tooltip',
-		href: '/docs/primitives/tooltip',
-		description:
-			'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-	},
-]
 
 const HeaderNav = () => {
 	return (
@@ -59,31 +20,16 @@ const HeaderNav = () => {
 				<NavigationMenuItem>
 					<NavigationMenuTrigger>Услуги</NavigationMenuTrigger>
 					<NavigationMenuContent>
-						<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-							<li className="row-span-3">
-								<NavigationMenuLink asChild>
-									<a
-										className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-										href="/"
-									>
-										<Logo />
-										<div className="mb-2 mt-4 text-lg font-medium">
-											shadcn/ui
-										</div>
-										<p className="text-sm leading-tight text-muted-foreground">
-											Beautifully designed components
-											built with Radix UI and Tailwind
-											CSS.
-										</p>
-									</a>
-								</NavigationMenuLink>
+						<ul className="">
+							<li className="">
+								<NavigationMenuLink asChild></NavigationMenuLink>
 							</li>
 						</ul>
 					</NavigationMenuContent>
 				</NavigationMenuItem>
 				<NavigationMenuItem>
 					<Link
-						href="/portfolio"
+						href="/works"
 						legacyBehavior
 						passHref
 					>
@@ -107,30 +53,5 @@ const HeaderNav = () => {
 		</NavigationMenu>
 	)
 }
-
-const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-	({ className, title, children, ...props }, ref) => {
-		return (
-			<li>
-				<NavigationMenuLink asChild>
-					<a
-						ref={ref}
-						className={cn(
-							'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-							className,
-						)}
-						{...props}
-					>
-						<div className="text-sm font-medium leading-none">{title}</div>
-						<p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-							{children}
-						</p>
-					</a>
-				</NavigationMenuLink>
-			</li>
-		)
-	},
-)
-ListItem.displayName = 'ListItem'
 
 export { HeaderNav }

20d4314df5924ba49e276556ed39fc4ef4f45f68
vadzim.l <vadzim.l@realmond.com>
2024-09-08 19:20:24 +0300

    delete: `framer-motion`

--- src/components/cookies/cookies-banner.tsx
+++ src/components/cookies/cookies-banner.tsx
@@ -5,64 +5,56 @@ import { Text } from '~/components/ui/text'
 import { Button } from '~/components/ui/button'
 
 type CookiesBannerProps = StyleProps & {
-	show?: boolean
 	confirm?: (event: React.FormEvent<HTMLFormElement>) => unknown
 }
 
-const CookiesBanner = ({ style, confirm, className, show = false }: CookiesBannerProps) => {
-	if (!show) {
-		return null
-	}
-
-	return (
-		<form
-			style={style}
-			className={cn(
-				'h-fit',
-				'mx-auto py-5 px-5 w-full',
-				'flex items-center gap-4',
-				'bg-white border-container shadow-brand !border-2',
-				'max-md:flex-col max-md:gap-y-3',
-				className,
-			)}
-			onSubmit={(event) => {
-				event.preventDefault()
-				confirm?.(event)
-			}}
+const CookiesBanner = ({ style, confirm, className }: CookiesBannerProps) => (
+	<form
+		style={style}
+		className={cn(
+			'h-fit',
+			'mx-auto py-5 px-5 w-full',
+			'flex items-center gap-4',
+			'bg-white border-container shadow-brand !border-2',
+			'max-md:flex-col max-md:gap-y-3',
+			className,
+		)}
+		onSubmit={(event) => {
+			event.preventDefault()
+			confirm?.(event)
+		}}
+	>
+		<Button
+			size="icon"
+			color="secondary"
+			className="self-start text-2xl"
 		>
-			<Button
-				size="icon"
-				color="secondary"
-				className="self-start text-2xl"
+			🍪
+		</Button>
+
+		<div className="self-start flex flex-col gap-y-1.5 max-md:mb-2">
+			<Text className="max-w-3xl text-base leading-snug cursor-default">
+				На нашем сайте мы обрабатываем cookie для улучшения вашего пользовательского опыта:
+			</Text>
+
+			<a
+				target="_blank"
+				href="/legal/cookies.pdf"
+				className="w-max min-h-max font-medium text-base text-link-default hover:text-link-active transition-all"
 			>
-				🍪
-			</Button>
+				Больше информации
+			</a>
+		</div>
 
-			<div className="self-start flex flex-col gap-y-1.5 max-md:mb-2">
-				<Text className="max-w-3xl text-base leading-snug cursor-default">
-					На нашем сайте мы обрабатываем cookie для улучшения вашего пользовательского
-					опыта:
-				</Text>
-
-				<a
-					target="_blank"
-					href="/legal/cookies.pdf"
-					className="w-max min-h-max font-medium text-base text-link-default hover:text-link-active transition-all"
-				>
-					Больше информации
-				</a>
-			</div>
-
-			<Button
-				size="sm"
-				type="submit"
-				className="ml-auto self-start py-2.5 max-md:w-full md:self-center px-4"
-			>
-				Понимаю
-			</Button>
-		</form>
-	)
-}
+		<Button
+			size="sm"
+			type="submit"
+			className="ml-auto self-start py-2.5 max-md:w-full md:self-center px-4"
+		>
+			Понимаю
+		</Button>
+	</form>
+)
 
 export { CookiesBanner }
 export type { CookiesBannerProps }
--- src/components/cookies/cookies.tsx
+++ src/components/cookies/cookies.tsx
@@ -1,22 +1,16 @@
 'use client'
 
 import { useCallback } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
 
 import { cn } from '~/lib/utils'
 import type { StyleProps } from '~/lib/types'
-import { TRANSTION_TRANSLATES } from '~/lib/framer-motion'
 
 import { useCookie } from '~/hooks/use-cookie'
 import { useResponsive } from '~/hooks/use-responsive'
 
 import { CookiesBanner } from './cookies-banner'
 
-type CookiesProps = StyleProps & {
-	hide?: boolean
-}
-
-const Cookies = ({ style, className, hide = false }: CookiesProps) => {
+const Cookies = ({ style, className }: StyleProps) => {
 	const [agreed, setAgreed] = useCookie<boolean>(COOKIES_AGREED_KEY)
 	const { isDesktop } = useResponsive()
 
@@ -27,34 +21,24 @@ const Cookies = ({ style, className, hide = false }: CookiesProps) => {
 		})
 	}, [setAgreed])
 
-	const show = !hide && !agreed
+	if (agreed) {
+		return null
+	}
 
 	return (
-		<AnimatePresence initial={show}>
-			{show ? (
-				<motion.div
-					key="cookies"
-					exit="exit"
-					initial="exit"
-					animate="enter"
-					variants={TRANSTION_TRANSLATES.bottom}
-					style={{
-						...style,
-						paddingBottom: isDesktop ? 20 : 0,
-					}}
-					className={cn(
-						'z-50 fixed inset-x-0 bottom-0 mx-auto md:max-w-3xl lg:max-w-5xl h-fit',
-						className,
-					)}
-				>
-					<CookiesBanner
-						show
-						confirm={confirm}
-					/>
-					<div className="absolute bg-black h-6 inset-x-0 -z-10 bottom-0 md:hidden" />
-				</motion.div>
-			) : null}
-		</AnimatePresence>
+		<div
+			key="cookies"
+			style={{
+				...style,
+				paddingBottom: isDesktop ? 20 : 0,
+			}}
+			className={cn(
+				'z-50 fixed inset-x-0 bottom-0 mx-auto md:max-w-3xl lg:max-w-5xl h-fit',
+				className,
+			)}
+		>
+			<CookiesBanner confirm={confirm} />
+		</div>
 	)
 }
 
@@ -62,4 +46,3 @@ const COOKIES_AGREED_KEY = 'cookies_agreed'
 const ONE_YEAR_IN_MS = 365 * 24 * 60 * 1000 * 1000
 
 export { Cookies }
-export type { CookiesProps }

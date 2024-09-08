72edf8ae892ea4abfc4ce96de4665e06b4132931
vadzim.l <vadzim.l@realmond.com>
2024-09-08 18:49:47 +0300

    add: `utils.types`

---
+++ src/lib/types.ts
@@ -0,0 +1,47 @@
+type nullable<T> = T | null | undefined
+type AnyObject = {
+	[x: string | number | symbol]: unknown
+}
+type ArrowFunction<P extends Array<unknown> = [], R = unknown> = (...args: P) => R
+
+type As = React.ElementType<any>
+type ReactPropsOf<T extends As> = React.ComponentPropsWithoutRef<T>
+type ReactChildren = React.ReactNode | React.ReactNode[] | undefined | null | string
+type MergeWithHTMLProps<T extends As, P = {}> = Omit<ReactPropsOf<T>, keyof P> &
+	P & {
+		as?: As
+	}
+
+type StyleProps = Partial<{
+	className: string
+	style: React.CSSProperties
+}>
+
+type Layout = (props: { children: ReactChildren }) => React.ReactNode
+
+type PageProps<P = {}, S = {}> = {
+	params: P
+	searchParams: S
+}
+type Page<P = {}, S = {}> = (
+	props: PageProps<P, S>,
+) => Promise<React.ReactElement<PageProps<P, S>>> | React.ReactElement<PageProps<P, S>>
+
+type HTMLImageProps = React.DetailedHTMLProps<
+	React.ImgHTMLAttributes<HTMLImageElement>,
+	HTMLImageElement
+>
+
+export type {
+	As,
+	Page,
+	Layout,
+	nullable,
+	AnyObject,
+	StyleProps,
+	ReactPropsOf,
+	ReactChildren,
+	ArrowFunction,
+	HTMLImageProps,
+	MergeWithHTMLProps,
+}

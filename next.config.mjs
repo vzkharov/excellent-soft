15e87b1be316e914b20eddea71c22a63c313b870
vadzim.l <vadzim.l@realmond.com>
2024-09-08 18:47:51 +0300

    configure:

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const config = {
+	compress: true,
+	swcMinify: true,
+	// output: 'export',
+	optimizeFonts: true,
+	reactStrictMode: true,
+	productionBrowserSourceMaps: true,
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+
+	typescript: {
+		ignoreBuildErrors: true,
+		tsconfigPath: './tsconfig.json',
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
+	headers: () => [],
+	rewrites: () => [{ source: '/services', destination: '/services/development' }],
+
+	images: {
+		minimumCacheTTL: 31536000,
+		dangerouslyAllowSVG: true,
+		formats: ['image/webp'],
+	},
+}
+
+const withMDX = createMDX({})
+
+// Merge MDX config with Next.js config
+export default withMDX(config)

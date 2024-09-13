84f5663daa01bd893c79772961e13024c7bff763
vadzim.l <vadzim.l@realmond.com>
2024-09-13 14:55:11 +0300

    add: `app-nav-config`

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,22 +1,51 @@
 type NavLinkConfig = {
+	id: string
 	href: string
 	name: string
 }
 
-type NavConfig = {
-	title: string
-	options: NavLinkConfig[]
-}
+const appNavConfig = {
+	index: {
+		id: 'index',
+		href: '/',
+		name: 'Главная',
+	},
+	works: {
+		id: 'works',
+		href: '/works',
+		name: 'Портфолио',
+	},
+	instalment: {
+		id: 'instalment',
+		href: '/instalment',
+		name: 'В рассрочку',
+	},
+	contact: {
+		id: 'contact',
+		href: '/contact',
+		name: 'Контакты',
+	},
+	services: {
+		id: 'services',
+		href: '/services',
+		name: 'Услуги',
+	},
+	design: {
+		id: 'design',
+		href: '/services/design',
+		name: 'Дизайн',
+	},
+	development: {
+		id: 'development',
+		href: '/services/development',
+		name: 'Веб-разработка',
+	},
+	promotion: {
+		id: 'promotion',
+		href: '/services/promotion',
+		name: 'Продвижение',
+	},
+} satisfies Record<string, NavLinkConfig>
 
-const APP_NAV_CONFIG = {
-	title: 'Навигация',
-	options: [],
-} as NavConfig
-
-const HEADER_NAV_CONFIG = {
-	title: '',
-	options: [],
-} as NavConfig
-
-export { APP_NAV_CONFIG, HEADER_NAV_CONFIG }
-export type { NavConfig, NavLinkConfig }
+export { appNavConfig }
+export type { NavLinkConfig }

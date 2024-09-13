10f92359fd4f0712d88dccacbd12ba4c5cdec100
vadzim.l <vadzim.l@realmond.com>
2024-09-13 19:15:55 +0300

    update: `footer` color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,12 @@
-import { Button } from '~/components/ui/button'
-
 import { Section } from '~/components/section'
-import { FeedSection } from '~/components/feed-section'
+import { FeedSection } from '~/components/(sections)/feed-section'
 
 const Footer = () => {
 	return (
 		<div>
 			<FeedSection />
 			<Section
-				bg="bg-content-200"
+				bg="bg-gray-200"
 				className="flex items-center justify-center p-9"
 			></Section>
 		</div>

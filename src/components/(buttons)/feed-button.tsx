ddb54cc028a0d84e4cf06b6c8d3bdc8477d419a7
vadzim.l <vadzim.l@realmond.com>
2024-09-14 18:09:18 +0300

    add: `feed-button`

---
+++ src/components/(buttons)/feed-button.tsx
@@ -0,0 +1,44 @@
+import { tv, type VariantProps } from 'tailwind-variants'
+
+import type { StyleProps } from '~/lib/types'
+import { feedConfig } from '~/config/feed'
+
+import { Button, type ButtonProps } from '~/components/ui/button'
+
+type FeedButtonProps = Pick<ButtonProps, 'size' | 'variant'> & FeedButtonVariants & StyleProps & {}
+
+const FeedButton = ({ size, variant, className, dir = 'x', ...props }: FeedButtonProps) => (
+	<div
+		{...props}
+		className={feedButtonVariants({ dir, className })}
+	>
+		{[feedConfig.telegram, feedConfig.whatsapp].map((feed) => (
+			<Button
+				key={feed.id}
+				size={size}
+				variant={variant}
+				/** @fix delete `className` */
+				className="group rounded-full border border-dark bg-transparent font-gilroy-bold text-sm text-dark hover:bg-dark hover:text-white"
+			>
+				{feed.name}
+			</Button>
+		))}
+	</div>
+)
+
+const feedButtonVariants = tv({
+	base: 'flex gap-x-3 gap-y-2',
+	variants: {
+		dir: {
+			x: 'flex-row items-center',
+			y: 'flex-col w-full',
+		},
+	},
+	defaultVariants: {
+		dir: 'x',
+	},
+})
+
+type FeedButtonVariants = VariantProps<typeof feedButtonVariants>
+
+export { FeedButton }

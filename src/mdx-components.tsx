9d25e72a78368bb6e081c0d7e9f1ffd45f3e3424
vadzim.l <vadzim.l@realmond.com>
2024-09-14 18:10:20 +0300

    update: default `mdx-components`

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,7 +2,12 @@ import type { MDXComponents } from 'mdx/types'
 
 export const useMDXComponents = (components: MDXComponents): MDXComponents => ({
 	strong: ({ children }) => <strong className="font-gilroy-bold">{children}</strong>,
+
 	ul: ({ children }) => <ul className="ml-8 list-disc">{children}</ul>,
 	ol: ({ children }) => <ol className="ml-8 list-decimal">{children}</ol>,
+
+	p: ({ children }) => <p className="max-w-prose">{children}</p>,
+	h6: ({ children }) => <h6 className="max-w-prose">{children}</h6>,
+	h5: ({ children }) => <h5 className="max-w-prose">{children}</h5>,
 	...components,
 })

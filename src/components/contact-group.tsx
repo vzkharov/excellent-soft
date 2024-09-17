52e6764f4bbba1e8e54bf981ed8404c94ae79b4c
vadzim.l <vadzim.l@realmond.com>
2024-09-18 00:36:48 +0300

    update: `contact-group` with label

--- src/components/contact-group.tsx
+++ src/components/contact-group.tsx
@@ -2,18 +2,25 @@ import { tv } from 'tailwind-variants'
 
 import { contactConfig } from '~/config/contact'
 
+import { Text } from '~/components/ui/text'
 import { Link } from '~/components/ui/link'
 import { Image } from '~/components/ui/image'
 
 type ContactGroupProps = React.HTMLAttributes<HTMLDivElement>
 
 const ContactGroup = ({ className, ...props }: ContactGroupProps) => (
-	<div
-		{...props}
-		className={styles.group({ className })}
-	>
-		{[contactConfig.instagram, contactConfig.behance, contactConfig.telegram, contactConfig.whatsapp].map(
-			(link) => (
+	<div className={styles.wrapper()}>
+		<Text>Мы в соц. сетях:</Text>
+		<div
+			{...props}
+			className={styles.group({ className })}
+		>
+			{[
+				contactConfig.instagram,
+				contactConfig.behance,
+				contactConfig.telegram,
+				contactConfig.whatsapp,
+			].map((link) => (
 				<Link
 					key={link.id}
 					href={link.href}
@@ -27,13 +34,14 @@ const ContactGroup = ({ className, ...props }: ContactGroupProps) => (
 						className={styles.img()}
 					/>
 				</Link>
-			),
-		)}
+			))}
+		</div>
 	</div>
 )
 
 const styles = tv({
 	slots: {
+		wrapper: 'flex flex-col gap-y-3',
 		group: 'flex items-center gap-x-5',
 		link: 'w-fit',
 		img: 'h-8 w-8 filter grayscale brightness-200 hover:grayscale-0 hover:brightness-100 transition-all',

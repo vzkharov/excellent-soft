4a0eba32616ae157587f344cd327539ba62d512a
vadzim.l <vadzim.l@realmond.com>
2024-09-18 23:37:09 +0300

    update: input variants

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react'
+import { tv, type VariantProps } from 'tailwind-variants'
 
-import { cn } from '~/lib/utils'
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & InputVariants
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => (
-	<input
-		type={type}
-		className={cn(
-			'flex h-11 w-full border-b border-gray-300 bg-transparent transition-colors placeholder:text-gray-700 focus-visible:border-white focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-			className,
-		)}
-		ref={ref}
-		{...props}
-	/>
-))
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+	({ className, type, variant = 'default', ...props }, ref) => (
+		<input
+			type={type}
+			className={inputVariants({ variant, className })}
+			ref={ref}
+			{...props}
+		/>
+	),
+)
 
 Input.displayName = '@excellent-soft/input'
+
+const inputVariants = tv({
+	base: 'flex h-11 w-full border-b border-gray-300 bg-transparent transition-colors placeholder:text-gray-700 focus-visible:border-white focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+	variants: {
+		variant: {
+			default: 'text-dark border-gray-700 placeholder:text-gray-300 focus-visible:border-dark',
+			foreground: 'text-white border-gray-300 placeholder:text-gray-700 focus-visible:border-white',
+		},
+	},
+	defaultVariants: {
+		variant: 'default',
+	},
+})
+
+type InputVariants = VariantProps<typeof inputVariants>
 
 export { Input }
 export type { InputProps }

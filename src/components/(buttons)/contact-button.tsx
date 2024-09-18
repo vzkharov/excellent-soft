466dca635215c21bd64815b23034c68589380a59
vadzim.l <vadzim.l@realmond.com>
2024-09-18 23:37:35 +0300

    add: `contact-button`

---
+++ src/components/(buttons)/contact-button.tsx
@@ -0,0 +1,45 @@
+import { tv } from 'tailwind-variants'
+
+import { Text } from '~/components/ui/text'
+import { Input } from '~/components/ui/input'
+import { Button, type ButtonProps } from '~/components/ui/button'
+import { Dialog, DialogTitle, DialogHeader, DialogTrigger, DialogContent, DialogFooter } from '~/components/ui/dialog'
+
+const ContactButton = (props: ButtonProps) => (
+	<Dialog>
+		<DialogTrigger asChild>
+			<Button {...props} />
+		</DialogTrigger>
+		<DialogContent className={styles.content()}>
+			<DialogHeader>
+				<DialogTitle>Давайте обсудим вашу идею!</DialogTitle>
+			</DialogHeader>
+
+			<div className={styles.body()}>
+				<Text>С нами можно связаться любым удобным способом:</Text>
+
+				<li>Написать нам в Telegram или WhatsApp</li>
+				<li>Либо мы вам перезвоним</li>
+			</div>
+
+			<DialogFooter>
+				<Input
+					type="tel"
+					name="phone"
+					color="default"
+					placeholder="Ваш номер телефона"
+				/>
+				<Button bold>Перезвонить мне</Button>
+			</DialogFooter>
+		</DialogContent>
+	</Dialog>
+)
+
+const styles = tv({
+	slots: {
+		content: 'px-10 pb-12 pt-10',
+		body: 'flex flex-col gap-y-3',
+	},
+})()
+
+export { ContactButton }

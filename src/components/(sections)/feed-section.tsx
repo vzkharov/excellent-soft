33e19f421dca7b0eb4bde56ae54ec78eaa4e1db9
vadzim.l <vadzim.l@realmond.com>
2024-09-18 23:38:54 +0300

    update: `feed-section`

--- src/components/(sections)/feed-section.tsx
+++ src/components/(sections)/feed-section.tsx
@@ -1,31 +1,20 @@
 import { tv } from 'tailwind-variants'
 
-import {
-	Dialog,
-	DialogTitle,
-	DialogHeader,
-	DialogTrigger,
-	DialogContent,
-	DialogDescription,
-} from '~/components/ui/dialog'
-
 import { Section } from '~/components/section'
+import { ContactButton } from '~/components/(buttons)/contact-button'
 
-/** @fix with button */
 const FeedSection = () => (
 	<Section
 		bg={styles.bg()}
 		className={styles.container()}
 	>
-		<Dialog>
-			<DialogTrigger className={styles.trigger()}>Давайте обсудим ваш проект</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>Свяжитесь с нами</DialogTitle>
-					<DialogDescription>Described By</DialogDescription>
-				</DialogHeader>
-			</DialogContent>
-		</Dialog>
+		<ContactButton
+			bold
+			size="lg"
+			variant="outlined"
+		>
+			Давайте обсудим ваш проект
+		</ContactButton>
 	</Section>
 )
 
@@ -33,7 +22,6 @@ const styles = tv({
 	slots: {
 		bg: 'bg-gradient-to-r from-gradient-start to-gradient-end',
 		container: 'flex items-center justify-center p-9',
-		trigger: 'group rounded-full font-gilroy-bold text-md border border-dark px-16 py-3 bg-transparent hover:bg-gray-0 text-gray-0 hover:text-gray-1',
 	},
 })()
 

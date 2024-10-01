d68240ed3ad47c9297d8548807800142dad6c6ae
vadzim.l <vadzim.l@realmond.com>
2024-10-01 20:26:38 +0300

    add: `contact-form`

---
+++ src/modules/contact-form/contact-form.tsx
@@ -0,0 +1,53 @@
+import { tv } from 'tailwind-variants'
+
+import { Input } from '~/components/ui/input'
+import { AgreementCheckbox } from '~/components/agreement-checkbox'
+
+import { HubSpotForm, SubmitButton, type HubSpotFormProps } from '~/modules/hub-spot-form'
+
+import { formConfig } from './utils'
+
+const ContactForm = ({ className, ...props }: HubSpotFormProps) => (
+	<HubSpotForm
+		{...props}
+		className={styles.form({ className })}
+	>
+		{formConfig.map((field) => (
+			<Input
+				key={field.name}
+				{...field}
+				variant="foreground"
+				className={styles.input()}
+			/>
+		))}
+
+		<AgreementCheckbox
+			required
+			id="agreement"
+			name="agreement"
+			variant="foreground"
+			className={styles.agreement()}
+		/>
+
+		<SubmitButton
+			bold
+			full
+			size="lg"
+			variant="gradient"
+			className={styles.submitButton()}
+		>
+			Отправить
+		</SubmitButton>
+	</HubSpotForm>
+)
+
+const styles = tv({
+	slots: {
+		form: 'flex flex-col',
+		input: 'mt-7 first-of-type:mt-0',
+		agreement: 'mt-4',
+		submitButton: 'py-3 mt-12',
+	},
+})()
+
+export { ContactForm }

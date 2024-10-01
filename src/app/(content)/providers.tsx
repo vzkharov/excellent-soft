77656baa9ea815dcf3cbf3fec336268f8c503ebb
vadzim.l <vadzim.l@realmond.com>
2024-10-01 20:27:10 +0300

    add: sonner

--- src/app/(content)/providers.tsx
+++ src/app/(content)/providers.tsx
@@ -1,9 +1,20 @@
 'use client'
 
+import { Toaster } from 'sonner'
+
 import type { Provider } from '~/lib/types'
 
 import { BodyColorProvider } from '~/modules/body-color-provider'
 
-const Providers: Provider = ({ children }) => <BodyColorProvider>{children}</BodyColorProvider>
+const Providers: Provider = ({ children }) => (
+	<BodyColorProvider>
+		{children}
+
+		<Toaster
+			richColors
+			position="bottom-center"
+		/>
+	</BodyColorProvider>
+)
 
 export { Providers }

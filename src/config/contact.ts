290e47b428b08184308be802d8ab6fcab21027e5
vadzim.l <vadzim.l@realmond.com>
2024-11-05 19:46:20 +0300

    update: contacts

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -7,12 +7,48 @@ type ContactConfig = {
 }
 
 const contactConfig = {
-	instagram: { id: 'instagram', name: 'Instagram', img: '/social-media/instagram.png', href: '', value: '' },
-	behance: { id: 'behance', name: 'Behance', img: '/social-media/behance.png', href: '', value: '' },
-	telegram: { id: 'telegram', name: 'Telegram', img: '/social-media/telegram.png', href: '', value: '' },
-	whatsapp: { id: 'whatsapp', name: 'WhatsApp', img: '/social-media/whatsapp.png', href: '', value: '' },
-	phone: { id: 'phone', name: 'Телефон', img: '', href: 'tel:+375296334212', value: '+375 29 633 42 12' },
-	email: { id: 'email', name: 'e-mail', img: '', href: 'mailto:hello@esoft.by', value: 'hello@esoft.by' },
+	instagram: {
+		id: 'instagram',
+		name: 'Instagram',
+		img: '/social-media/instagram.png',
+		href: '',
+		value: '',
+	},
+	behance: {
+		id: 'behance',
+		name: 'Behance',
+		img: '/social-media/behance.png',
+		href: '',
+		value: '',
+	},
+	telegram: {
+		id: 'telegram',
+		name: 'Telegram',
+		img: '/social-media/telegram.png',
+		href: 'https://t.me/+375291882527',
+		value: '@esoft',
+	},
+	whatsapp: {
+		id: 'whatsapp',
+		name: 'WhatsApp',
+		img: '/social-media/whatsapp.png',
+		href: 'whatsapp://send?phone=+375291882527&abid=+375291882527',
+		value: '@esoft',
+	},
+	phone: {
+		id: 'phone',
+		name: 'Телефон',
+		img: '',
+		href: 'tel:+375291882527',
+		value: '+375 29 188 25 27',
+	},
+	email: {
+		id: 'email',
+		name: 'e-mail',
+		img: '',
+		href: 'mailto:hello@esoft.by',
+		value: 'hello@esoft.by',
+	},
 } satisfies Record<string, ContactConfig>
 
 export { contactConfig }
